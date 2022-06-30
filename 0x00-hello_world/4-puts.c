de788f903dfd02eef17b527e13c484449bcbfd12
M1R2T1N <martinmusaking@gmail.com>
2022-06-30 14:53:57 -0700

    third commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
- #include <stdio.h>
+#include <stdio.h>
 
 /**
- * main entry point
+ * main - Entry point
  *
- * return: always 0 (success)
+ * return: Always 0 (Success)
  */
- int main(void)
- {
-	 puts("\"Programming is like building a multilingual puzzle");
-	   return (0);
- }
+int main(void)
+{
+	puts("\"Programming is like building a multiligual puzzle");
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,5 @@
-Readme low level programming
+0. a script that runs a C file in preprocesor and save result into another file
+1. a script that compiles a C file but does not link 
+2. a script that generates the assembly code of a C code and save it in another output file
+3. a script that compiles a C file and creates an exucatable file name cisfun
+4.
