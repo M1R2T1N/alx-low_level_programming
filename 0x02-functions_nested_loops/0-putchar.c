2cd81119f1f2b8b54505dec7f8ee5b1aeef9f544
M1R2T1N <martinmusaking@gmail.com>
2022-07-06 19:33:44 -0700

    second commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,16 @@
  */
 int main(void)
 {
-	char *sh = "main";
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
+	_putchar("_");
+	_putchar("p");
+	_putcahr("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
 
 	return (0);
 }
+
 
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+/**
+ * -putchar - writes the character c to stdout
+ *  @c: Tthe character to print
+ *
+ *  Return: On success 1.
+ *  On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+#include <unistd.h>
+
+/**
+ * -putchar - writes the characterc to stdout
+ *  @c: The character to print
+ *
+ *  Return: On success 1.
+ *  On errror, -1 is returned, and errno is set appropriately.
+ */
+int -putchar(char c)
+{
+	return (write(1, &c, 1));
+}}
+	
