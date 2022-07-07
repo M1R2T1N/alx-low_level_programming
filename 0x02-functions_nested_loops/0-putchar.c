e59d08bdbfb82cbd596ef4c84ecbdbaee6b7354e
M1R2T1N <martinmusaking@gmail.com>
2022-07-06 19:58:46 -0700

    third commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putcahr("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
+	_putchar('_');
+	_putchar('p');
+	_putcahr('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
 	return (0);
 }
