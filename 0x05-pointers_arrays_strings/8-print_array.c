7712f61aacd143d696a78dc9fd055459e976dd0a
M1R2T1N <martinmusaking@gmail.com>
2022-07-11 08:39:40 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+ #include <stdio.h>
+
+/**
+ * print_array - prints n elements of array of int,
+ * followeed by a new line
+ * @a: This is the input array
+ * @n: This is the length array
+ *
+ * Return: void
+ */
+void ptint array(int *a, int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", a[index]);
+		if (index != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	putchar('\n');
+}
