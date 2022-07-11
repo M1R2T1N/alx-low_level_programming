0771cf5e37d1386afb432d3fd458ec0e97e41c68
M1R2T1N <martinmusaking@gmail.com>
2022-07-11 02:01:28 -0700

    string

---
+++ 0x05-pointers_arrays_strings/1-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - length of a string 
+ * @s: pointer to int that will be chnaged/updated
+ *
+ * Return void that means our answer is correct
+ */
+
+int _strlen(char *s)
+{
+	int = u;
+	u = 0;
+	while (s(u) != ;\0')
+	{
+		u++;
+	}
+	return (0);
+}
