f8383c0f8f268ba5c51bfbab9d77b1aba9d2579a
M1R2T1N <martinmusaking@gmail.com>
2022-07-14 00:07:12 -0700

    task 0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,10 +1,15 @@
-#ifndef HEADER_FILE
-#define HEADRER_FILE
-
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+/**
+ * void prototypes()
+ * int prototypes()
+ * char prototypes()
+ */
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
-char *strncat(char *dest, char *src, int n);
-char *strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
@@ -13,4 +18,8 @@ char *leet(char *);
 char *rot13(char *);
 char *_strstr(char *haystack, char *needle);
 void *print_number(int n);
-#endif
+void *print_buffer(char *b, int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_string(char *n1, char *n2, char *r, int r_index);
+
+#endif /*MAIN_H*/
