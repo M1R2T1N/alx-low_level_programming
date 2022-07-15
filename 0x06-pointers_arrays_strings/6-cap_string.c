942a3b3956b3614c2f8269363a0056c0989062f6
M1R2T1N <martinmusaking@gmail.com>
2022-07-15 14:42:26 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,35 +1,35 @@
-#include "holberton.h"
-	
-	/**
-	 * cap_string - Write a function that capitalizes all words of a string.
-	 *
-	 * @entry: This is the input string
-	 *
-	 * Return: String capitalized
-	 */
-	char *cap_string(char *entry)
+#include "main.h"
+
+/**
+ * cap_string - Write a function that capitalizes first character of a word
+ *
+ * @str: This is the input string to capitalize
+ *
+ * Return: String capitalized
+ */
+char *cap_string(char *str)
+{
+	int index = 0;
+
+	while (str[++index])
 	{
-		int conversion, index, count;
-	
-		char chars[] = {' ', ',', ';', '.', '!',
-				 '?', '"', '(', ')', '{', '}',  '\t', '\n', '\0'};
-		conversion = 32;
-	
-		for (index = 0; entry[index] != '\0'; index++)
-		{
-			if (entry[index] >= 'index' && entry[index] <= 'z')
-			{
-				entry[index] =  entry[index] - conversion;
-			}
-			conversion = 0;
-			for (count = 0; chars[count] != '\0'; count++)
-			{
-				if (chars[count] == entry[index])
-				{
-					conversion = 32;
-					break;
-				}
-			}
-		}
-		return (entry);
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
+				str[index - 1] == '.' ||
+				str[index - 1] == '!' ||
+				str[index - 1] == '?' ||
+				str[index - 1] == '"' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				str[index - 1] == '{' ||
+				str[index - 1] == '}')
+			str[index] -= 32;
 	}
+	return (str);
+}
