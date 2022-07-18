d643ba71589c6dee226005ea07d7ef5b7805487c
M1R2T1N <martinmusaking@gmail.com>
2022-07-18 07:28:58 -0700

    task 4

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strspn - gets lengthof a prefix substring
+ * @s: string to check
+ * @accept: string to check against
+ *
+ * Retrun: number of bytes of s in accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, k, counter = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+
+		if (counter != i)
+			break;
+	for (k = 0; accept[k] != '\0'; k++)
+	{
+		if (s[i] == accept[k])
+			counter++;
+	}
+
+return (counter);
+}
