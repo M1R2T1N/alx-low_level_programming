502b7f5192fc51a45dfa782ea80d476904397775
M1R2T1N <martinamusaking@gmail.com>
2022-10-20 02:34:26 -0700

    commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,14 +2,13 @@
 
 /**
  * print_number - prints an integer
- *
  * @n: integer to be printed
- *
  */
 
 void print_number(int n)
 {
 	unsigned int n1;
+
 	if (n < 0)
 	{
 		n1 = -n;
@@ -18,9 +17,11 @@ void print_number(int n)
 	{
 		n1 = n;
 	}
+
 	if (n1 / 10)
 	{
 		print_number(n1 / 10);
 	}
+
 	_putchar((n1 % 10) + '0');
 }
